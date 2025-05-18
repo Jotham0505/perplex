3a5ab3ae49b2722cd9a393e43117eec77f3f2b35
Jotham Emmanuel Cheeran <jothamemmanuelcheeran@gmail.com>
2025-05-18 18:06:13 +0530

    UI Done

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.submitButton),
         scaffoldBackgroundColor: AppColors.background,
         textTheme: GoogleFonts.interTextTheme(ThemeData.dark().textTheme),
       ),
--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:perplex_clone/theme/colors.dart';
+import 'package:perplex_clone/widgets/search_section.dart';
 import 'package:perplex_clone/widgets/side_bar.dart';
 
 class Homepage extends StatelessWidget {
@@ -12,12 +14,75 @@ class Homepage extends StatelessWidget {
         //side nav bar
         SideBar(),
 
-        Column(
-          children: [
-            // serach section
+        Expanded(
+          child: Column(
+            children: [
+              // serach section
+              Expanded(child: SearchSection()),
+              // footer
 
-            // footer
-          ],
+              Container(
+                padding: EdgeInsets.symmetric(
+                  vertical: 16,
+                ),
+                child: Wrap(
+                  alignment: WrapAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 12,
+                      ),
+                      child: Text(
+                        "Pro",
+                        style: TextStyle(
+                            fontSize: 14, color: AppColors.footerGrey),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 12,
+                      ),
+                      child: Text(
+                        "Enterprise",
+                        style: TextStyle(
+                            fontSize: 14, color: AppColors.footerGrey),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 12,
+                      ),
+                      child: Text(
+                        "Store",
+                        style: TextStyle(
+                            fontSize: 14, color: AppColors.footerGrey),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 12,
+                      ),
+                      child: Text(
+                        "Blog",
+                        style: TextStyle(
+                            fontSize: 14, color: AppColors.footerGrey),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 12,
+                      ),
+                      child: Text(
+                        "English(English)",
+                        style: TextStyle(
+                            fontSize: 14, color: AppColors.footerGrey),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         )
       ],
     ));
