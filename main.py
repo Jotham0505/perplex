1f43991983c10e89d4fe8b1eb46e989b88437b28
Jotham Emmanuel Cheeran <jothamemmanuelcheeran@gmail.com>
2025-08-04 01:24:37 +0530

    web sockets backend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+import asyncio
+from fastapi import FastAPI, WebSocket
 
 from pydantic_models.chat_body import ChatBody
 from services.llm_service import LLMService
@@ -11,7 +12,38 @@ search_service = SearchService()
 sort_source_service = SortSourceService()
 llm_service = LLMService()
 
-# chat 
+# chat websocket
+@app.websocket("/ws/chat")
+async def websocket_char_endpoint(websocket: WebSocket):
+    await websocket.accept()
+
+    try:
+        await asyncio.sleep(0.1)
+        data = await websocket.receive_json()
+        query = data.get("query")
+
+        search_results = search_service.web_search(query)
+        sorted_results = sort_source_service.sort_sources(query,search_results)
+        await asyncio.sleep(0.1)
+        await websocket.send_json({
+            "type" : "search_result",
+            "data" : sorted_results
+        })
+    
+        for chunk in llm_service.generate_response(query,sorted_results):
+            await asyncio.sleep(0.1)
+            await websocket.send_json({
+                "type":"content",
+                "data":chunk
+            })
+
+    except:
+        print("Unexpected error occured")
+    finally:
+        await websocket.close()
+
+
+
 @app.post("/chat") # we need to take the body of the chat, and we have to use POST
 def chat_endpoint(body: ChatBody):
 
--- services/llm_service.py
+++ services/llm_service.py
@@ -24,6 +24,7 @@ class LLMService:
         Think and reason deeply. Ensure it answers the query the user is asking. Do not use your knowledge until it is absolutely necessary.
         """
        
-       response = self.model.generate_content(full_prompt)
+       response = self.model.generate_content(full_prompt,stream=True)
 
-       return response.text
+       for chunk in response:
+           yield chunk.text
