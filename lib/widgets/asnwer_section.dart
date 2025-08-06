b9570153faa5b6e45ca5b0bad0e224dfad031d1b
Jotham Emmanuel Cheeran <jothamemmanuelcheeran@gmail.com>
2025-08-07 00:11:07 +0530

    skeletonizer

--- lib/widgets/asnwer_section.dart
+++ lib/widgets/asnwer_section.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:perplex_clone/services/chat_web_service.dart';
 import 'package:perplex_clone/theme/colors.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class AsnwerSection extends StatefulWidget {
   const AsnwerSection({super.key});
@@ -11,14 +12,31 @@ class AsnwerSection extends StatefulWidget {
 }
 
 class _AsnwerSectionState extends State<AsnwerSection> {
-  String fullResponse = '';
+  bool isLoading = true;
+  String fullResponse = '''
+The purple fox danced across the sky while humming an ancient melody only the stars could understand. 
+Beneath the glittering trees, marbles rolled uphill and whispers filled the air with stories of forgotten moons and peanut butter robots. 
+Everyone agreed that Tuesdays smelled like cinnamon when reality took a lunch break.
+
+The purple fox danced across the sky while humming an ancient melody only the stars could understand. 
+Beneath the glittering trees, marbles rolled uphill and whispers filled the air with stories of forgotten moons and peanut butter robots. 
+Everyone agreed that Tuesdays smelled like cinnamon when reality took a lunch break.
+
+The purple fox danced across the sky while humming an ancient melody only the stars could understand. 
+Beneath the glittering trees, marbles rolled uphill and whispers filled the air with stories of forgotten moons and peanut butter robots. 
+Everyone agreed that Tuesdays smelled like cinnamon when reality took a lunch break.
+''';
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     ChatWebService().contentStream.listen((data) {
+      if (isLoading) {
+        fullResponse = "";
+      }
       setState(() {
         fullResponse += data['data'];
+        isLoading = false;
       });
     });
   }
@@ -32,16 +50,20 @@ class _AsnwerSectionState extends State<AsnwerSection> {
           "Perplexity",
           style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
         ),
-        Markdown(
-          data: fullResponse,
-          shrinkWrap: true,
-          styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
-            codeblockDecoration: BoxDecoration(
-              color: AppColors.cardColor,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            code: TextStyle(
-              fontSize: 14,
+        Skeletonizer(
+          enabled: isLoading,
+          child: Markdown(
+            data: fullResponse,
+            shrinkWrap: true,
+            styleSheet:
+                MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
+              codeblockDecoration: BoxDecoration(
+                color: AppColors.cardColor,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              code: TextStyle(
+                fontSize: 14,
+              ),
             ),
           ),
         ),
--- lib/widgets/sources_section.dart
+++ lib/widgets/sources_section.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:perplex_clone/services/chat_web_service.dart';
 import 'package:perplex_clone/theme/colors.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class SourcesSection extends StatefulWidget {
   const SourcesSection({super.key});
@@ -10,7 +11,25 @@ class SourcesSection extends StatefulWidget {
 }
 
 class _SourcesSectionState extends State<SourcesSection> {
-  List searchResults = [];
+  bool isLoading = true;
+  List searchResults = [
+    {
+      'title': 'Ind vs Aus Live Score 4th Test',
+      'url':
+          'https://www.moneycontrol.com/sports/cricket/ind-vs-aus-live-score-4th-test'
+    },
+    {
+      'title': 'Ind vs Aus Live Boxing Day Test',
+      'url':
+          'https://timesofindia.indiatimes.com/sports/cricket/india-vs-australia-live-boxing-day-test'
+    },
+    {
+      'title': 'Ind vs Aus - 4 Australian Batters Score Half Centuries',
+      'url':
+          'https://economictimes.indiatimes.com/news/sports/ind-vs-aus-four-australian-batters-score-half-centuries'
+    },
+  ];
+
   @override
   void initState() {
     // TODO: implement initState
@@ -18,6 +37,7 @@ class _SourcesSectionState extends State<SourcesSection> {
     ChatWebService().searchResultStream.listen((data) {
       setState(() {
         searchResults = (data['data']);
+        isLoading = false;
       });
     });
   }
@@ -49,48 +69,51 @@ class _SourcesSectionState extends State<SourcesSection> {
         SizedBox(
           height: 14,
         ),
-        Wrap(
-          runSpacing: 10,
-          spacing: 10,
-          children: [
-            ...searchResults.map((res) {
-              return Container(
-                width: 140,
-                height: 115,
-                decoration: BoxDecoration(
-                  color: AppColors.cardColor,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        res['title'],
-                        style: TextStyle(fontWeight: FontWeight.w500),
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Text(
-                        res['url'],
-                        style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          color: Colors.grey,
-                          fontSize: 12,
-                        ),
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ],
+        Skeletonizer(
+          enabled: isLoading,
+          child: Wrap(
+            runSpacing: 10,
+            spacing: 10,
+            children: [
+              ...searchResults.map((res) {
+                return Container(
+                  width: 140,
+                  height: 115,
+                  decoration: BoxDecoration(
+                    color: AppColors.cardColor,
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                ),
-              );
-            }).toList(),
-          ],
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          res['title'],
+                          style: TextStyle(fontWeight: FontWeight.w500),
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          res['url'],
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: Colors.grey,
+                            fontSize: 12,
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }).toList(),
+            ],
+          ),
         )
       ],
     );
